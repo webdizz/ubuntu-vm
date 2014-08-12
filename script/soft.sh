936e2ef2f1e192dcbde48fdaefba676a20436484
Izzet Mustsfaiev <webdizz@gmail.com>
2014-08-12 09:43:39 +0300

    Increase HDD size + additional soft

---
+++ script/soft.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get -y install software-properties-common python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get -y upgrade
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get -y install oracle-java8-installer
+apt-get clean
+update-alternatives --display java
+
+apt-get -y install vim mysql-client wget tcpdump sshpass htop python-pip ansible
+pip install -U fig
+
+wget -O - http://gaudi.io/apt/gaudi.gpg.key | sudo apt-key add -
+echo "deb http://gaudi.io/apt/ precise main" | sudo tee -a /etc/apt/sources.list
+ 
+sudo apt-get update
+sudo apt-get install gaudi -y
+
